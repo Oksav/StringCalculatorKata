e08dd3dea30682d3176e7ffe4dd02b6d50b6ef54
oksav <oktag@DESKTOP-72AO56J>
2021-05-26 17:57:28 +0200

    refactor

--- StringCalculatorKata/StringCalculator_Add.cs
+++ StringCalculatorKata/StringCalculator_Add.cs
@@ -6,11 +6,10 @@ namespace StringCalculatorKata
 
     public class StringCalculator_Add
     {
+        StringCalculator calculator = new StringCalculator();
         [Fact]
         public void Returns0GivenEmptyString()
         {
-            var calculator = new StringCalculator();
-
             var result = calculator.Add("");
 
             Assert.Equal(0, result);
@@ -21,8 +20,6 @@ namespace StringCalculatorKata
         [InlineData("2",2)]
         public void ReturnsNumberGivenStringWithNumbers(string number, int expectedResult)
         {
-            var calculator = new StringCalculator();
-
             var result = calculator.Add(number);
 
             Assert.Equal(expectedResult, result);
@@ -30,10 +27,9 @@ namespace StringCalculatorKata
 
         [Theory]
         [InlineData("1,2", 3)]
+        [InlineData("4,5", 9)]
         public void ReturnsSumGivenStringWithTwoCommaSeparatedNumbers(string number, int expectedResult)
         {
-            var calculator = new StringCalculator();
-
             var result = calculator.Add(number);
 
             Assert.Equal(expectedResult, result);
