d74a8d93dc1504c7815e93bdce0495225b2ba9d1
oksav <oktag@DESKTOP-72AO56J>
2021-05-26 16:21:39 +0200

    Add project files.

---
+++ StringCalculatorKata/StringCalculator_Add.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace StringCalculatorKata
+{
+
+    public class StringCalculator_Add
+    {
+        [Fact]
+        public void Returns0GivenEmptyString()
+        {
+
+        }
+    }
+}
